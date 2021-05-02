cae5356e242e943cc2e61b0b0a7b495ab0d41610
Narendra Badi <badinarendra46@gmail.com>
2021-05-02 16:01:00 +0530

    Creditcard in ICICI

---
+++ src/main/java/icici/config/Service.java
@@ -0,0 +1,13 @@
+package icici.config;
+
+import lombok.Data;
+
+/**
+ * @author <a href="mailto:JGraham@aimconsulting.com">Justin Graham</a>
+ * @since 2/15/16
+ */
+@Data
+public class Service {
+    private String host;
+    private String version;
+}
---
+++ src/main/java/icici/config/options/KnownServices.java
@@ -0,0 +1,10 @@
+package icici.config.options;
+
+/**
+ * @author <a href="mailto:JGraham@aimconsulting.com">Justin Graham</a>
+ * @since 2/15/16
+ */
+public enum KnownServices {
+    USER,
+    AUTH
+}
